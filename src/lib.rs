1506707c5e311411f78c6a0f2383dd3b38090339
antonovmike <antonovma@outlook.com>
2024-01-26 21:21:42 +0400

    first

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
+use std::{ffi::CStr, str::Utf8Error};
+
 #[no_mangle]
 pub extern "C" fn print_text(text: *const std::os::raw::c_char) {
-    // Convert a pointer to a C string to a Rust string
-    let c_str = unsafe { std::ffi::CStr::from_ptr(text) };
-    // Try to convert Rust string to UTF-8
-    let result = c_str.to_str();
-    // If conversion is successful, output the string to the terminal
+    // Convert a pointer to a C string to a borrowed C string
+    let c_str: &CStr = unsafe { std::ffi::CStr::from_ptr(text) };
+    // Try to convert the borrowed C string string to UTF-8
+    let result: Result<&str, Utf8Error> = c_str.to_str();
+
     if let Ok(string) = result {
         println!("{}", string);
     }
