a7879c8282b20a1ea82c427f78d49a4644de16eb
antonovmike <antonovma@outlook.com>
2024-01-27 14:15:15 +0400

    database read and write

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import ctypes
 
 
-def read_file(filename):
-    with open(filename, 'r') as file:
-        data = file.read()
-    return data
+name = input("Enter your name: ")
+birth_year = input("Enter your birth year: ")
 
-
-filename = "example.txt"
-text = read_file(filename)
 rustlib = ctypes.cdll.LoadLibrary("target/release/librust_ffi.so")
-rustlib.print_text(text.encode("utf-8"))
+rustlib.print_person(name.encode("utf-8"), birth_year.encode("utf-8"))
